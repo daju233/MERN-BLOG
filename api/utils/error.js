9bfe9e13119db4b4d353e88202982dc22b299946
DA-JU <2212509690@qq.com>
2024-05-08 01:17:36 +0800

    修改errorhandle

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,8 @@
-export default errorHandler = (statusCode, message) => {
-  const error = new Error();
-  errror.statusCode = statusCode;
-  error.message = message;
+export const errorHandler = (statusCode, message) => {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  //   error.message = message;
+  //注意：在 JavaScript 中，通常 Error 对象的 message 属性用于存储错误的描述信息，但是由于 Error 构造函数接受一个字符串作为可选参数，这个字符串通常会被用作 message 属性的初始值。在上面的 errorHandler 函数中，我已经将 message 参数传递给了 Error 构造函数。但是，您仍然可以通过 error.message = message; 来覆盖默认的 message（虽然这通常不是必要的）。
+
   return error;
 };
