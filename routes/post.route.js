d92ae51f6cc288b4a7fb96fd930a4f1ce2344b7a
DA-JU <2212509690@qq.com>
2024-05-23 16:58:41 +0800

    create post api

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyToken } from "../api/utils/verifyUser.js";
-import { createPost } from "../api/controllers/post.conroller.js";
+import { createPost, getPosts } from "../api/controllers/post.conroller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createPost);
+router.get("/getposts", getPosts);
 
 export default router;
